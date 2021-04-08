1b2516837224e34f4f8429e785e5fa076e3caad9
matiiv <matiiv.r.a@gmail.com>
2021-04-08 08:37:20 +0300

    start write interface for courier and order

---
+++ rest_api_delivery_service/courier_manager.py
@@ -0,0 +1,11 @@
+
+
+class CourierManager:
+    def __init__(self):
+        raise NotImplemented
+
+    def add_new_couriers(self):
+        raise NotImplemented
+
+    def change_courier_info(self):
+        raise NotImplemented
---
+++ rest_api_delivery_service/order_manager.py
@@ -0,0 +1,8 @@
+
+
+class OrderManager:
+    def __init__(self):
+        pass
+
+    def add_new_orders(self):
+        raise NotImplemented
