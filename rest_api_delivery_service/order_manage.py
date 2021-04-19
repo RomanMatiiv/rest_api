1004ab27a4cf3b64ffd98a1c6568596fc925d90f
matiiv <matiiv.r.a@gmail.com>
2021-04-19 08:27:42 +0300

    write interface for order manage

---
+++ rest_api_delivery_service/order_manage.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+class OrderManage:
+    def __init__(self):
+        raise NotImplemented
+
+    def add_new_order(self, order_id: int):
+        raise NotImplemented
+
+    def mark_order_done(self, order_id: int):
+        raise NotImplemented
