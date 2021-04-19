93e4dba28702951ed92804c237012f572826450a
matiiv <matiiv.r.a@gmail.com>
2021-04-19 08:27:26 +0300

    write interface for courier manage

---
+++ rest_api_delivery_service/courier_manage.py
@@ -0,0 +1,43 @@
+"""
+
+"""
+
+
+class CourierManage:
+    def __init__(self):
+        raise NotImplemented
+
+    def add_new_courier(self, courier_id: int):
+        """
+        Добавляет нового курьера в систему
+
+        Args:
+            courier_id: уникальный номер курьера в системе
+
+        Returns:
+        """
+        raise NotImplemented
+
+    def change_courier(self, courier_id: int):
+        """
+        Изменяет информацию у сществующего курьера
+
+        Args:
+            courier_id: уникальный номер курьера в системе
+
+        Returns:
+
+        """
+        raise NotImplemented
+
+    def assign_orders(self, courier_id: int):
+        """
+        Назначает на курьера заказы
+
+        Args:
+            courier_id:
+
+        Returns:
+
+        """
+        raise NotImplemented
