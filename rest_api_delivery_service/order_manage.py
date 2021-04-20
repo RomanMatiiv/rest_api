36042f68eef548b22f7e306c883454ee7fa5d9a1
matiiv <matiiv.r.a@gmail.com>
2021-04-20 07:30:54 +0300

    write documentation

--- rest_api_delivery_service/order_manage.py
+++ rest_api_delivery_service/order_manage.py
@@ -1,13 +1,32 @@
 """
-
+В данном модуле реализованна логика работы с заказами
 """
+
 
 class OrderManage:
     def __init__(self):
         raise NotImplemented
 
     def add_new_order(self, order_id: int):
+        """
+        Добавляет новый заказ в систему
+
+        Args:
+            order_id: уникальныйи номер заказа в системе
+
+        Returns:
+
+        """
         raise NotImplemented
 
     def mark_order_done(self, order_id: int):
+        """
+        Помечает заказ как выполненый
+
+        Args:
+            order_id: уникальный номер заказа в системе
+
+        Returns:
+
+        """
         raise NotImplemented
